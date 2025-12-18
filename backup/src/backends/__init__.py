c8f5da799b77b74d6d523a275ef881d6e7a57749
Apollo <jorgecabrejas7@gmail.com>
2025-12-18 14:06:47 +0100

    chore: remove old backup directory and its contents.

--- backup/src/backends/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Backends Package - Emulator communication implementations.
-
-Currently supports:
-- emerald: Pokemon Emerald via mGBA Lua connector
-"""
-
-from __future__ import annotations
-
